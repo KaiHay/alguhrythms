e63c6e63a5b54e1d77130ee6e8e745a8f24b5c7e
Kai Hay <kai@mac.mynetworksettings.com>
2025-06-24 11:27:04 -0400

    animate so hard :(

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { use, useEffect, useState } from "react";
 import { radixSort, type fullRadMoves, type radLogs } from "./_components/radix";
+import {motion} from "motion/react"
 
 export default function Home() {
   const [masterArr, setMaster] = useState([''])
@@ -8,8 +9,11 @@ export default function Home() {
   const [currDigit, setDigit] = useState(0)
   const [currNumber, setCurrNumber] = useState('')
   const [stepC, setStep] = useState(0)
+  const fullMoveList = radixSort(['3', '1', '100000', '50'])
   useEffect(() => {
-    const fullMoveList = radixSort(['3', '1', '10', '50'])
+    if (!fullMoveList.moves[stepC]) {
+      return
+    }
     setMaster(fullMoveList.moves[stepC]!.masterArray)
     setBuckets(fullMoveList.moves[stepC]!.buckets)
     setDigit(fullMoveList.moves[stepC]!.currDigit)
@@ -25,8 +29,31 @@ export default function Home() {
   }
   return (
     <div className="flex justify-center flex-col text-center">
-      <div className="">{masterArr}</div>
-      <div className="">{buckets}</div>
+      <div className="flex flex-row justify-center">
+        {masterArr.map((val, idx) => {
+          return (
+            <div key={idx} className="p-1">
+              {masterArr[idx] !== undefined && buckets.some(bucket => bucket.includes(masterArr[idx]!)) ? '_' : masterArr[idx] ?? ''}
+            </div>
+          )
+        })}
+      </div>
+      <div className="">
+        <motion.div style={{originX:1}} className="flex fixed bottom-10 w-full justify-evenly  mt-8">
+          {buckets.map((val, idx) => {
+            return (
+              <div className='' key={idx}>
+                <div className="">
+                  <div className={`${buckets[idx]!.length === 0 ? 'opacity-0' : ''}`}>
+                    {buckets[idx]!.length === 0 ? 'd' : buckets[idx]!.join(' ')}
+                  </div>
+                  <div className=" border border-t-transparent w-10">{idx}</div>
+                </div>
+              </div>
+            )
+          })}
+        </motion.div>
+      </div>
       <div className="">{currDigit}</div>
       <div className="">{currNumber}</div>
       <button className='border bg-amber-500' onClick={() => incrementStep()}>poopy</button>
