1f2074592fc05a8ad40445f00dbdaa5a94f0995d
Kai Hay <kai@Kais-MacBook-Air.local>
2025-06-25 11:17:30 -0400

    some alg stuff + dfs func

---
+++ src/app/_components/adv.tsx
@@ -0,0 +1,17 @@
+import * as fs from 'fs';
+const file = fs.readFileSync('src/app/_components/input.txt', 'utf8');
+const hi = file.split('\n')
+const group = []
+for (let i = 0; i < hi.length; i++) {
+    const units = hi[i]!.split(/\s+/)
+    group.push(units)
+}
+let right=0
+let left = 0
+for (let i = 0;  i< group.length; i++){
+    console.log(i)
+    left += parseInt(group[i]![0]!) 
+    right += parseInt(group[i]![1]!)
+    
+}
+console.log(Math.abs(left-right));
